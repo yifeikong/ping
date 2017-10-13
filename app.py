404dac827d8633ce9ed3447ecc8a663463c88a34
Yifei Kong <kongyifei@gmail.com>
2017-10-13 20:32:43 +0800

    add verify proxy function

--- app.py
+++ app.py
@@ -13,6 +13,21 @@ async def ping(request):
     ret['headers'] = request.headers
     return json(ret)
 
+@app.route('/verify_proxy')
+async def verify_proxy(request):
+    ip = request.ip[0]
+    proxy_ip = request.args.get('proxy_ip')
+    if ip != proxy_ip:
+        return json({'status': 'error', 'ip': ip, 'message': 'proxy ip does not match request ip'})
+    if 'x-forwarded-for' in request.headers:
+        anonymous_type = 'low'
+    elif 'proxy-connection' in request.headers or 'via' in request.headers:
+        anonymous_type = 'middle'
+    else:
+        anonymous_type = 'high'
+    return json({'status': 'ok', 'ip': ip, 'message': 'proxy success', 'anonymous_type': anonymous_type})
+
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
